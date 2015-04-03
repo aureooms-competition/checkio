733665985a58d1e10aeca0c4850620058b135055
aureooms <aurelien.ooms@gmail.com>
2015-04-03 09:56:29 +0200

    open-labyrinth

--- home/open-labyrinth.py
+++ home/open-labyrinth.py
@@ -1,112 +1,81 @@
-#Your code here
-#You can import some modules or create additional functions
+
+class FifoQueue ( object ) :
+
+	class Node ( object ) :
+
+		def __init__ ( self , item , prev , next ) :
+
+			self.item = item
+			self.prev = prev
+			self.next = next
+
+	def __init__ ( self ) :
+
+		self.head = FifoQueue.Node( None , None , None )
+		self.tail = FifoQueue.Node( None , None , None )
+
+		self.head.next = self.tail
+		self.tail.prev = self.head
+
+	def empty ( self ) :
+
+		return self.head.next == self.tail
+
+	def put ( self , item ) :
+
+		node = FifoQueue.Node( item , self.tail.prev , self.tail )
+
+		node.prev.next = node
+		self.tail.prev = node
+
+	def get ( self ) :
+
+		node = self.head.next
+
+		self.head.next = node.next
+		node.next.prev = self.head
+
+		return node.item
+
+
+def neighbours ( m , n , i , j ) :
+
+	if j + 1 < n : yield "E" , ( i , j + 1 )
+	if i + 1 < m : yield "S" , ( i + 1 , j )
+	if j > 0 : yield "W" , ( i , j - 1 )
+	if i > 0 : yield "N" , ( i - 1 , j )
+
+
+def bfs ( used , m , n , source , sink ) :
+
+	queue = FifoQueue( )
+
+	i , j = source
+
+	used[i][j] = 1
+
+	queue.put( ( "" , i , j ) )
+
+	while not queue.empty( ) :
+
+		path , i , j = queue.get( )
+
+		for direction , position in neighbours ( m , n , i , j ) :
+
+			if position == sink : return path + direction
+
+			x , y = position
+
+			if used[x][y] : continue
+
+			used[x][y] = 1
+
+			queue.put( ( path + direction , x , y ) )
+
 
 def checkio ( maze ) :
 
-    #replace this for solution
-    #This is just example for first maze
-    return "SSSSSEENNNEEEEEEESSWWWWSSSEEEESS"
+	source = ( 1 , 1 )
+	sink = ( 10 , 10 )
 
-
-if __name__ == '__main__':
-    #This code using only for self-checking and not necessary for auto-testing
-    def check_route(func, labyrinth):
-        MOVE = {"S": (1, 0), "N": (-1, 0), "W": (0, -1), "E": (0, 1)}
-        #copy maze
-        route = func([row[:] for row in labyrinth])
-        pos = (1, 1)
-        goal = (10, 10)
-        for i, d in enumerate(route):
-            move = MOVE.get(d, None)
-            if not move:
-                print("Wrong symbol in route")
-                return False
-            pos = pos[0] + move[0], pos[1] + move[1]
-            if pos == goal:
-                return True
-            if labyrinth[pos[0]][pos[1]] == 1:
-                print("Player in the pit")
-                return False
-        print("Player did not reach exit")
-        return False
-
-    # These assert are using only for self-testing as examples.
-    assert check_route(checkio, [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
-        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
-        [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "First maze"
-    assert check_route(checkio, [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Empty maze"
-    assert check_route(checkio, [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Up and down maze"
-    assert check_route(checkio, [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Dotted maze"
-    assert check_route(checkio, [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-        [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1],
-        [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
-        [1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-        [1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "Need left maze"
-    assert check_route(checkio, [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-        [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]), "The big dead end."
-    print("The local tests are done.")
+	return bfs( maze , 12 , 12 , source , sink )
