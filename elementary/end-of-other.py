455da22b595dcce0d61afe3ab5e57f5e5bb6b182
aureooms <aurelien.ooms@gmail.com>
2015-04-03 21:24:36 +0200

    comments for elementary/end-of-other

--- elementary/end-of-other.py
+++ elementary/end-of-other.py
@@ -9,6 +9,12 @@ class Trie ( object ) :
 
 	def insert ( self , string , i , j , data ) :
 
+		"""
+			Inserts string[i:j] in the trie. Satellite data can be attached
+			through parameter `data`. Cannot insert empty strings using this
+			method.
+		"""
+
 		if i >= j : return
 
 		node = Trie( data if i == j - 1 else None )
@@ -21,6 +27,11 @@ class Trie ( object ) :
 
 
 	def longestprefix ( self , string , i , j ) :
+
+		"""
+			Returns where a string should be inserted in the trie and what
+			range of character would have to be inserted.
+		"""
 
 		if i >= j : return ( i , self )
 
@@ -35,18 +46,38 @@ class Trie ( object ) :
 
 def checkio ( words ) :
 
+	"""
+		O(n), where n is the sum of the number of characters in each string of
+		the input set `words`.
+	"""
+
+	# We will use this trie to store suffixes of words. A node in this trie
+	# will have satellite data set to `True` iff this node corresponds to the
+	# end of a word of the input set.
 	trie = Trie( None )
 
 	for word in words :
 
-		drow = word[::-1]
+		# `drow` is the reversed word. Prefixes of reversed words are suffixes
+		# of the original words.
+		n , drow = len( word ) , word[::-1]
 
-		n = len( drow )
-
+		# This call returns where `drow` could be inserted in the trie.
+		# `i` is the position of the first character of `drow` that should be
+		# inserted in the trie. `node` is where we should insert this `i`th
+		# character.
 		i , node = trie.longestprefix( drow , 0 , n )
 
+		# If the position where we should insert is a whole reversed word, i.e.
+		# `node.data` is `True`, or if our reversed word is the prefix of a
+		# reversed word already in the trie, i.e. `i` is `n`, then we must
+		# conclude that the input contains two words from which one is the
+		# suffix of the other.
 		if node.data or i == n : return True
 
+		# Otherwise we just insert the end of the reversed word in the trie.
+		# The satellite data of the last inserted node will contain the Boolean
+		# value `True`.
 		node.insert( drow , i , n , True )
 
 	return False
