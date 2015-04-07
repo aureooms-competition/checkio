df765d48643ad30ac5efc394d530747f9ede1e51
aureooms <aurelien.ooms@gmail.com>
2015-04-07 09:46:12 +0200

    13

--- elementary/13-days-diff.py
+++ elementary/13-days-diff.py
@@ -15,15 +15,17 @@
 #Precondition: Dates between 1 january 1 and 31 december 9999. Dates are
 #correct.
 
+import datetime
 
-def days_diff(date1, date2):
-    """
-        Find absolute diff in days between dates
-    """
-    return 0
+def days_diff ( date1 , date2 ) :
+
+	date1 = datetime.date( *date1 )
+	date2 = datetime.date( *date2 )
+
+	return abs( date1 - date2 ).days
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
-    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
-    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+	#These "asserts" using only for self-checking and not necessary for auto-testing
+	assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+	assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+	assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
