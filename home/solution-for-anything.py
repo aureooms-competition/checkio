d62d7c3af25f76b83675da2f1b745ecf61ac5fcd
aureooms <aurelien.ooms@gmail.com>
2015-06-30 10:26:23 +0000

    clean

--- home/solution-for-anything.py
+++ home/solution-for-anything.py
@@ -1,14 +1,12 @@
-class solution ( object ) :
+class checkio ( object ) :
 
+    def __init__ ( self , data ) : pass
     def __eq__ ( self , other ) : return True
     def __ne__ ( self , other ) : return True
     def __le__ ( self , other ) : return True
     def __lt__ ( self , other ) : return True
     def __ge__ ( self , other ) : return True
     def __gt__ ( self , other ) : return True
-
-def checkio ( anything ) :
-    return solution( )
 
 
 if __name__ == '__main__':
