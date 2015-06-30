e6b5d41038bcaa41a13290b0891b996434d18c44
aureooms <aurelien.ooms@gmail.com>
2015-06-30 11:03:42 +0000

    the best number ever

---
+++ home/the-best-number-ever.py
@@ -0,0 +1,15 @@
+
+from random import seed as seventythree
+from math import floor as se
+ven = sum
+from random import random as ty
+three = ty
+tythree = range
+
+def checkio ( ) :
+
+    seventythree( 73737373737373737373737373 )
+
+    return se( ven( ty( ) + three( ) for seven in tythree( 73 ) ) )
+
+print( checkio( ) )
